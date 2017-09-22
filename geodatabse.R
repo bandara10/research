a98f7118a37422a85597f94d68c7eb7441f14e2e
Ravi Dissanayake <disabandara@gmail.com>
2017-09-22 21:01:08 +1000

    google view

--- geodatabse.R
+++ geodatabse.R
@@ -20,3 +20,16 @@ lnd84 <- readRDS('data/LGA')
 leaflet() %>%
   addTiles() %>%
   addPolygons(data = LGA)
+
+library(mapview)
+mapView(hefleydata.presence)
+
+library(rgdal)
+library(maptools)
+writeOGR(LGA, dsn="LGA_study_areashp.kml", layer= "LGA", driver="KML")
+# lets covert hefleydata as kml to see it in google earth.
+BNG<- CRS("+init=epsg:28356") 
+proj4string(hefleydata.presence)<-BNG
+p4s <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84")
+hefleydata.presence <- spTransform (hefleydata.presence, p4s)
+writeOGR(hefleydata.presence, dsn="hefleydata.presence.kml", layer= "hefleydata.presence", driver="KML")
