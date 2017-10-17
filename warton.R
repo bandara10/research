ada5275d57ded93068e5cdb9a4ea5f16e71341be
Ravi Dissanayake <disabandara@gmail.com>
2017-10-17 16:17:37 +1000

    Three scenarios added

--- warton.R
+++ warton.R
@@ -2,36 +2,35 @@ library(ppmlasso)
 data(BlueMountains)
 # Pre-standardise observer bias variables
 backg.env = BlueMountains$env
-head(backg.env)
+#head(backg.env)
 xydatan <- backg.env[c(1,2)] # get x y colomns to be cbind later with predicted valuves.
-xydata <- backg.env[c(1,2)]
-coordinates(xydata) <- ~X+Y
-plot(xydata)
+#xydata <- backg.env[c(1,2)]
+#coordinates(xydata) <- ~X+Y
+#plot(xydata)
 
-stand.D_MAIN_RDS = backg.env$D_MAIN_RDS
-stand.D_URBAN = backg.env$D_URBAN
+#stand.D_MAIN_RDS = backg.env$D_MAIN_RDS
+#stand.D_URBAN = backg.env$D_URBAN
 #stand.D_MAIN_RDS = standardise.X(backg.env$D_MAIN_RDS)$X
 stand.D_MAIN_RDS=scale.default(backg.env$D_MAIN_RDS, center = TRUE, scale = TRUE) #standarise
 stand.D_URBAN=scale.default(backg.env$D_URBAN, center = TRUE, scale = TRUE) #standarise
 
 
 
-stand.D_MAIN_RDS # set distance to zero
-stand.D_URBAN
-backg.env$D_MAIN_RDS = stand.D_MAIN_RDS
-backg.env$D_URBAN = stand.D_URBAN
+#stand.D_MAIN_RDS # set distance to zero
+#stand.D_URBAN
+#backg.env$D_MAIN_RDS = stand.D_MAIN_RDS
+#backg.env$D_URBAN = stand.D_URBAN
 # To fit a Poisson point process model at a spatial resolution of 1km
-ppmForm = ~ poly(TMP_MAX,TMP_MIN, RAIN_ANN, degree = 2) 
+ppmForm = ~ poly(FC, TMP_MAX,TMP_MIN, RAIN_ANN, degree = 2) 
 ppmFit = ppmlasso(ppmForm, sp.xy = BlueMountains$eucalypt,
                   env.grid = backg.env, sp.scale = 1)
-summary(ppmForm)
-summary(ppmFit)
+
 # To predict using model-based control of observer bias (at min value for D_MAIN_RDS):
 newEnv = BlueMountains$env
 #newEnv$D_MAIN_RDS = min(stand.D_MAIN_RDS)
-pred.biasCorrect = predict(ppmFit, newdata=newEnv)
-predictions <- cbind(xydatan, pred.biasCorrect)
-xy.rr <- rasterFromXYZ(as.data.frame(predictions)[, c("X", "Y", "pred.biasCorrect")])
+pred.biasCorrect.1 = predict(ppmFit, newdata=newEnv)
+predictions <- cbind(xydatan, pred.biasCorrect.1)
+xy.rr <- rasterFromXYZ(as.data.frame(predictions)[, c("X", "Y", "pred.biasCorrect.1")])
 plot(xy.rr, las=0)
 # To find the resolution (in the range from 0.5 to 16 km):
 scales = c(0.5, 1, 2, 4, 8, 16)
@@ -47,17 +46,17 @@ resid.plot = diagnose(ppmFit, which = "smooth", type = "Pearson")
 # Pre-standardise observer bias variables
 backg.env = BlueMountains$env
 head(backg.env)
-stand.D_MAIN_RDS = backg.env$D_MAIN_RDS
-stand.D_URBAN = backg.env$D_URBAN
+#stand.D_MAIN_RDS = backg.env$D_MAIN_RDS
+#stand.D_URBAN = backg.env$D_URBAN
 #stand.D_MAIN_RDS = standardise.X(backg.env$D_MAIN_RDS)$X
 stand.D_MAIN_RDS=scale.default(backg.env$D_MAIN_RDS, center = TRUE, scale = TRUE) #standarise
 stand.D_URBAN=scale.default(backg.env$D_URBAN, center = TRUE, scale = TRUE) #standarise
-stand.D_MAIN_RDS # set distance to zero
-stand.D_URBAN
+#stand.D_MAIN_RDS # set distance to zero
+#stand.D_URBAN
 backg.env$D_MAIN_RDS = stand.D_MAIN_RDS
 backg.env$D_URBAN = stand.D_URBAN
 # To fit a Poisson point process model at a spatial resolution of 1km
-ppmForm.2 = ~ poly(TMP_MAX,TMP_MIN, RAIN_ANN, degree = 2) + poly(D_MAIN_RDS, D_URBAN, degree = 2)
+ppmForm.2 = ~ poly(FC,TMP_MAX,TMP_MIN, RAIN_ANN, degree = 2) + poly(D_MAIN_RDS, D_URBAN, degree = 2)
 ppmFit.2 = ppmlasso(ppmForm.2, sp.xy = BlueMountains$eucalypt,
                   env.grid = backg.env, sp.scale = 1)
 summary(ppmForm.2)
@@ -65,10 +64,10 @@ summary(ppmFit.2)
 # To predict using model-based control of observer bias (at min value for D_MAIN_RDS):
 newEnv = BlueMountains$env
 #newEnv$D_MAIN_RDS = min(stand.D_MAIN_RDS, D_URBAN)
-pred.biasCorrecta = predict(ppmFit.2, newdata=newEnv)
-head(pred.biasCorrect)
-predictions.2 <- cbind(xydatan, pred.biasCorrecta)
-xy.rr.2 <- rasterFromXYZ(as.data.frame(predictions.2)[, c("X", "Y", "pred.biasCorrecta")])
+pred.biasCorrect.2 = predict(ppmFit.2, newdata=newEnv)
+head(pred.biasCorrect.2)
+predictions.2 <- cbind(xydatan, pred.biasCorrect.2)
+xy.rr.2 <- rasterFromXYZ(as.data.frame(predictions.2)[, c("X", "Y", "pred.biasCorrect.2")])
 plot(xy.rr.2, las=0)
 # To find the resolution (in the range from 0.5 to 16 km):
 scales = c(0.5, 1, 2, 4, 8, 16)
@@ -81,7 +80,9 @@ resid.plot.2 = diagnose(ppmFit.2, which = "smooth", type = "Pearson")
 
 #######
 # To fit a Poisson point process model at a spatial resolution of 1km
-
+library(ppmlasso)
+data(BlueMountains)
+backg.env = BlueMountains$env
 stand.D_MAIN_RDS = backg.env$D_MAIN_RDS
 stand.D_URBAN = backg.env$D_URBAN
 #stand.D_MAIN_RDS = standardie.X(backg.env$D_MAIN_RDS)$X
@@ -90,18 +91,17 @@ stand.D_URBAN=scale.default(backg.env$D_URBAN, center = TRUE, scale = TRUE) #sta
 backg.env$D_MAIN_RDS = stand.D_MAIN_RDS
 backg.env$D_URBAN = stand.D_URBAN
 
-ppmForm.3 = ~ poly(TMP_MAX,TMP_MIN, RAIN_ANN, degree = 2) + poly(D_MAIN_RDS, D_URBAN, degree = 2)
+ppmForm.3 = ~ poly(FC,TMP_MAX,TMP_MIN, RAIN_ANN, degree = 2) + poly(D_MAIN_RDS, D_URBAN, degree = 2)
 ppmFit.3 = ppmlasso(ppmForm.3, sp.xy = BlueMountains$eucalypt,
                   env.grid = backg.env, sp.scale = 1)
-summary(ppmForm)
-summary(ppmFit)
+
 # To predict using model-based control of observer bias (at min value for D_MAIN_RDS):
 newEnv = BlueMountains$env
 newEnv$D_MAIN_RDS = min(stand.D_MAIN_RDS)
-newEnv$D_URBAN = min(stand.D_URBAN)
-pred.biasCorrectaa = predict(ppmFit.3, newdata=newEnv)
-predictions.3 <- cbind(xydatan, pred.biasCorrectaa)
-xy.rr.3 <- rasterFromXYZ(as.data.frame(predictions.3)[, c("X", "Y", "pred.biasCorrectaa")])
+newEnv$D_URBAN = min(stand.D_MAIN_RDS)
+pred.biasCorrect.3 = predict(ppmFit.3, newdata=newEnv)
+predictions.3 <- cbind(xydatan, pred.biasCorrect.3)
+xy.rr.3 <- rasterFromXYZ(as.data.frame(predictions.3)[, c("X", "Y", "pred.biasCorrect.3")])
 plot(xy.rr.3,asp=1)
 # To find the resolution (in the range from 0.5 to 16 km):
 scales = c(0.5, 1, 2, 4, 8, 16)
