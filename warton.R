c7fb62629eab3324a0f5f724f40c994c36f10157
Ravi Dissanayake <disabandara@gmail.com>
2017-10-17 18:44:10 +1000

    standarised observarion bias covariates

--- warton.R
+++ warton.R
@@ -11,11 +11,11 @@ xydatan <- backg.env[c(1,2)] # get x y colomns to be cbind later with predicted 
 #stand.D_MAIN_RDS = backg.env$D_MAIN_RDS
 #stand.D_URBAN = backg.env$D_URBAN
 #stand.D_MAIN_RDS = standardise.X(backg.env$D_MAIN_RDS)$X
+## Pre-standardise observer bias variables
 stand.D_MAIN_RDS=scale.default(backg.env$D_MAIN_RDS, center = TRUE, scale = TRUE) #standarise
 stand.D_URBAN=scale.default(backg.env$D_URBAN, center = TRUE, scale = TRUE) #standarise
-
-
-
+backg.env$D_MAIN_RDS = stand.D_MAIN_RDS
+backg.env$D_URBAN = stand.D_MAIN_RDS
 #stand.D_MAIN_RDS # set distance to zero
 #stand.D_URBAN
 #backg.env$D_MAIN_RDS = stand.D_MAIN_RDS
@@ -27,6 +27,7 @@ ppmFit = ppmlasso(ppmForm, sp.xy = BlueMountains$eucalypt,
 
 # To predict using model-based control of observer bias (at min value for D_MAIN_RDS):
 newEnv = BlueMountains$env
+head(newEnv)
 #newEnv$D_MAIN_RDS = min(stand.D_MAIN_RDS)
 pred.biasCorrect.1 = predict(ppmFit, newdata=newEnv)
 predictions <- cbind(xydatan, pred.biasCorrect.1)
@@ -51,8 +52,7 @@ head(backg.env)
 #stand.D_MAIN_RDS = standardise.X(backg.env$D_MAIN_RDS)$X
 stand.D_MAIN_RDS=scale.default(backg.env$D_MAIN_RDS, center = TRUE, scale = TRUE) #standarise
 stand.D_URBAN=scale.default(backg.env$D_URBAN, center = TRUE, scale = TRUE) #standarise
-#stand.D_MAIN_RDS # set distance to zero
-#stand.D_URBAN
+
 backg.env$D_MAIN_RDS = stand.D_MAIN_RDS
 backg.env$D_URBAN = stand.D_URBAN
 # To fit a Poisson point process model at a spatial resolution of 1km
@@ -72,7 +72,7 @@ plot(xy.rr.2, las=0)
 # To find the resolution (in the range from 0.5 to 16 km):
 scales = c(0.5, 1, 2, 4, 8, 16)
 findres(scales, sp.xy = BlueMountains$eucalypt,
-        env.grid = BlueMountains$env, formula = ppmForm)
+        env.grid = BlueMountains$env, formula = ppmForm.2)
 #which returns the log-likelihood at each scale, difference < 2 at 1km scale
 # Diagnostic plots as in Fig 5:
 kenv.2 = envelope(ppmFit.2, fun = Kinhom)
@@ -83,14 +83,12 @@ resid.plot.2 = diagnose(ppmFit.2, which = "smooth", type = "Pearson")
 library(ppmlasso)
 data(BlueMountains)
 backg.env = BlueMountains$env
-stand.D_MAIN_RDS = backg.env$D_MAIN_RDS
-stand.D_URBAN = backg.env$D_URBAN
-#stand.D_MAIN_RDS = standardie.X(backg.env$D_MAIN_RDS)$X
+## Pre-standardise observer bias variables
 stand.D_MAIN_RDS=scale.default(backg.env$D_MAIN_RDS, center = TRUE, scale = TRUE) #standarise
 stand.D_URBAN=scale.default(backg.env$D_URBAN, center = TRUE, scale = TRUE) #standarise
 backg.env$D_MAIN_RDS = stand.D_MAIN_RDS
-backg.env$D_URBAN = stand.D_URBAN
-
+backg.env$D_URBAN = stand.D_MAIN_RDS
+## To fit a Poisson point process model at a spatial resolution of 1km
 ppmForm.3 = ~ poly(FC,TMP_MAX,TMP_MIN, RAIN_ANN, degree = 2) + poly(D_MAIN_RDS, D_URBAN, degree = 2)
 ppmFit.3 = ppmlasso(ppmForm.3, sp.xy = BlueMountains$eucalypt,
                   env.grid = backg.env, sp.scale = 1)
@@ -111,8 +109,9 @@ findres(scales, sp.xy = BlueMountains$eucalypt,
 # Diagnostic plots as in Fig 5:
 kenv.3 = envelope(ppmFit.3, fun = Kinhom)
 resid.plot.3 = diagnose(ppmFit.3, which = "smooth", type = "Pearson")
-
-
-
-
-
+plot(xy.rr, las=0)
+plot(xy.rr.2, las=0)
+plot(xy.rr.3, las=0)
+resid.plot = diagnose(ppmFit, which = "smooth", type = "Pearson")
+resid.plot.2 = diagnose(ppmFit.2, which = "smooth", type = "Pearson")
+resid.plot.3 = diagnose(ppmFit.3, which = "smooth", type = "Pearson")
