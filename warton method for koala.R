b12f91093becda8a3e0de86252bf719fd2a69cd4
Ravi Dissanayake <disabandara@gmail.com>
2017-10-22 20:21:36 +1000

    new variable added

--- warton method for koala.R
+++ warton method for koala.R
@@ -63,13 +63,17 @@ str(env.v)
 kolaxy <- read.csv("wartondata\\kolaxy.csv", header = TRUE)
 kolaxy <- kolaxy[,1:ncol(kolaxy)]/1000 # selects every row and 2nd to last columns
 
+coordinates(kolaxy)= ~X+Y
+plot(kolaxy, add=TRUE)
+
+points(kolaxy$X, kolaxy$Y, pch = 15, col = "red")
 mydata <- list(koala=kolaxy ,  env.var=env.v)
 save(mydata, file="mydata.RData")
 load(file="mydata.RData")
 mydata$koala
 
-ppmForms = ~  poly(A, A.1, degree = 2)
-ppmFits = ppmlasso(ppmForms, sp.xy = kolaxy, env.grid = env.v, sp.scale = 1.5)
+ppmForm = ~  poly(A, A.1, degree = 2)
+ppmFit = ppmlasso(ppmForms, sp.xy = kolaxy, env.grid = env.v, sp.scale = 1.5)
 # To predict using model-based control of observer bias (at min value for D_MAIN_RDS):
 newEnv = BlueMountains$env
 head(newEnv)
@@ -86,3 +90,66 @@ findres(scales, sp.xy = kolaxy,
 # Diagnostic plots as in Fig 5:
 kenv = envelope(ppmFits, fun = Kinhom)
 resid.plot = diagnose(ppmFits, which = "smooth", type = "Pearson")
+
+
+
+#
+#lapply outputs a list of rasters
+# m is data matrix from getvalues stack.
+#r is an empry raster
+ll <- getValues(myenv.stack)
+lll <- as.data.frame(ll)
+
+#create an empty raster
+start_y <- 441829 #100
+start_x <- 6901098 #200
+griddf <- expand.grid(X = seq(from = start_y, by = 1, l = 100),
+                      Y = seq(from = start_x, by = 1, l = 100))
+
+A <- rnorm(10000, 55, 6)
+B <- rnorm(10000, 28, 20)
+
+envs<- cbind(griddf,A,B)
+
+xy.rr <- rasterFromXYZ(as.data.frame(envs)[, c("X", "Y", "A")])
+
+####
+l <- lapply(1:ncol(lll), function(i) {
+ setValues(xy.rr,lll[ , i])
+} )
+
+# stack the list
+s <- stack(l)
+ss <- as.data.frame(s, xy=TRUE)
+ss[is.na(ss)] <- 0
+colnames(ss)[1] <- "X"
+colnames(ss)[2] <- "Y"
+
+
+## Pre-standardise observer bias variables
+stand.A.1=scale.default(ss$A.1, center = TRUE, scale = TRUE) #standarise
+stand.A.2=scale.default(ss$A.2, center = TRUE, scale = TRUE) #standarise
+ss$A.1 = stand.A.1
+ss$A.2 = stand.A.2
+
+#########+ poly (A.3, A.4, degree = 2)
+ppmForm = ~  poly(A.3, A.4, degree = 2) 
+ppmFit = ppmlasso(ppmForm, sp.xy = kolaxy, env.grid = ss, sp.scale = 1)
+# To predict using model-based control of observer bias (at min value for D_MAIN_RDS):
+newEnv = ss
+newEnv$A.1 = min(stand.A.1)
+pred.biasCorrect = predict(ppmFit, newdata=newEnv)
+#newEnv$D_MAIN_RDS = min(stand.D_MAIN_RDS)
+pred.biasCorrect.1 = predict(ppmFit, newdata=ss)
+predictions <- cbind(xydatan, pred.biasCorrect.1)
+xy.rr <- rasterFromXYZ(as.data.frame(predictions)[, c("x", "y", "pred.biasCorrect.1")])
+plot(xy.rr, las=0)
+# To find the resolution (in the range from 0.5 to 16 km):
+scales = c(0.5, 1, 2, 4, 8, 16)
+findres(scales, sp.xy = kolaxy,
+        env.grid = env.v, formula = ppmForms)
+#which returns the log-likelihood at each scale, difference < 2 at 1km scale
+# Diagnostic plots as in Fig 5:
+kenv = envelope(ppmFits, fun = Kinhom)
+resid.plot = diagnose(ppmFits, which = "smooth", type = "Pearson")
+
