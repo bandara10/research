9e0dc5a762bb244830ff429259e21beed3318e9f
Ravi Dissanayake <disabandara@gmail.com>
2017-10-23 11:32:32 +1000

    algorithm did not converge

--- warton method for koala.R
+++ warton method for koala.R
@@ -30,8 +30,8 @@ A <- rnorm(10000, 0, 0)
 envs<- cbind(griddf,A)
 
 xy.rr <- rasterFromXYZ(as.data.frame(envs)[, c("X", "Y", "A")])
-proj4string(xy.rr) <- CRS("+init=epsg:28356")
-xy.rr[is.na(xy.rr[])] <- 0
+#proj4string(xy.rr) <- CRS("+init=epsg:28356")
+#xy.rr[is.na(xy.rr[])] <- 0 # this raster is fine but includs the sea.
  
 plot(xy.rr)
 ####
@@ -43,14 +43,18 @@ l <- lapply(1:ncol(lll), function(i) {
 s <- stack(l)
 plot(s)
 ss <- as.data.frame(s, xy=TRUE)
-ss[is.na(ss)] <- 0
+#ss[is.na(ss)] <- 0
 colnames(ss)[1] <- "X"
 colnames(ss)[2] <- "Y"
+sss <- na.omit(ss)
+xydatan <- sss[c(1,2)] # to create a raster
 
 
-xydatan <- ss[c(1,2)]
+xydatan <- na.omit(xydatan)
 #koala data
 kolaxy <- read.csv("wartondata\\kolaxy.csv", header = TRUE)
+kolaxy <- kolaxy[sample(nrow(kolaxy), 80), ]
+
 #kolaxy <- kolaxy[,1:ncol(kolaxy)]/1000 # selects every row and 2nd to last columns
 #coordinates(kolaxy)= ~ X+Y
 #proj4string(kolaxy) <- CRS("+init=epsg:28356")
@@ -62,21 +66,21 @@ kolaxy <- read.csv("wartondata\\kolaxy.csv", header = TRUE)
 #ss$A.2 = stand.A.2
 
 #########+ +poly (A.1, degree = 2)
-ppmForm = ~  poly(A.1, A.2, degree = 1) 
-ppmFit = ppmlasso(ppmForm, sp.xy = kolaxy, env.grid = ss, sp.scale = 1)
+ppmForm = ~  poly(A.1, A.2, degree = 2) 
+ppmFit = ppmlasso(ppmForm, sp.xy = kolaxy, env.grid = sss, sp.scale = 1)
 # To predict using model-based control of observer bias (at min value for D_MAIN_RDS):
-#newEnv = ss
+newEnv = sss
 #newEnv$A.1 = min(stand.A.1)
 pred.biasCorrect = predict(ppmFit, newdata=newEnv)
 #newEnv$A.1 = min(stand.A.1)
-pred.biasCorrect.1 = predict(ppmFit, newdata=ss)
+pred.biasCorrect.1 = predict(ppmFit, newdata=sss)
 predictions <- cbind(xydatan, pred.biasCorrect.1)
 xy.rr <- rasterFromXYZ(as.data.frame(predictions)[, c("X", "Y", "pred.biasCorrect.1")])
 plot(xy.rr, las=0)
 # To find the resolution (in the range from 0.5 to 16 km):
 scales = c(0.5, 1, 2, 4, 8, 16)
 findres(scales, sp.xy = kolaxy,
-        env.grid = ss, formula = ppmForm)
+        env.grid = sss, formula = ppmForm)
 #which returns the log-likelihood at each scale, difference < 2 at 1km scale
 # Diagnostic plots as in Fig 5:
 kenv = envelope(ppmFit, fun = Kinhom)
