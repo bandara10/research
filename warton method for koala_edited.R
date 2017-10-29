6e33e99efe1dcd71dc73d944613501a04ec473f5
Ravi Dissanayake <disabandara@gmail.com>
2017-10-29 18:21:41 +1000

    model works wih res .5

--- warton method for koala_edited.R
+++ warton method for koala_edited.R
@@ -15,7 +15,7 @@ myenv.stack <- stack(myenv)
 # myenv.stack2 <- stack.c
 
 d <- (myenv.stack[[1]])
-e <- (myenv.stack[[2]])
+k <- (myenv.stack[[2]])
 f <- (myenv.stack[[3]])
 
 e <- as.data.frame(d, xy=TRUE)
@@ -30,27 +30,27 @@ eee <- ee [c(3,4)]
 # ar <- rasterFromXYZ(as.data.frame(a)[, c("x", "y", "elev")])
 
 dd <- as.data.frame(d)
-ee<- as.data.frame(e)
+kk<- as.data.frame(k)
 ff<- as.data.frame(f)
-rasterXY <- cbind(eee$X, eee$Y, dd$distance_tertiaryandlink, ee$elev, ff$temp)
+rasterXY <- cbind(eee$X, eee$Y, dd$distance_tertiaryandlink, kk$elev, ff$temp)
 rasterXYa <- as.data.frame(rasterXY)
 colnames(rasterXYa)[1] <- "X"
 colnames(rasterXYa)[2] <- "Y"
-colnames(rasterXYa)[3] <- "TEM"
-colnames(rasterXYa)[4] <- "ELEV"
-colnames(rasterXYa)[5] <- "TEM"
+colnames(rasterXYa)[3] <- "ELEV"
+colnames(rasterXYa)[4] <- "TEMP"
 
 #rasterXYaX <- rasterXYa$X/1000 
 #rasterXYa$Y <- rasterXYa$Y/1000
-arr <- rasterFromXYZ(as.data.frame(rasterXYa)[, c("X", "Y", "TEM")], res=.5)
-arrr <- rasterFromXYZ(as.data.frame(rasterXYa)[, c("X", "Y", "ELEV")], res=.5)
+arr <- rasterFromXYZ(as.data.frame(rasterXYa)[, c("X", "Y", "TEMP")], res=1)
+arrr <- rasterFromXYZ(as.data.frame(rasterXYa)[, c("X", "Y", "ELEV")], res=1)
 st <- stack(arr, arrr)
+res(st) <- .5
+plot(st[[1]])
 
 stt <- as.data.frame(st, xy=TRUE)
-
 colnames(stt)[1] <- "X"
 colnames(stt)[2] <- "Y"
-
+stt[is.na(stt)] <- .0001
 
 # quadrature points not working.
 quad.1 = sample.quad(env.grid =stt , sp.scale = 1, file = "Quad")
@@ -58,34 +58,38 @@ quad.1 = sample.quad(env.grid =stt , sp.scale = 1, file = "Quad")
 
 #koala data
 kolaxy <- read.csv("wartondata\\kolaxy.csv", header = TRUE)
-kolaxy <- subset(kolaxy, y> 6901098 & y < 7000000, select=x:y) # limit y selection
+kxy <- subset(kolaxy, y> 6951098 & y < 6991098, select=x:y) # limit y selection
 
-# minimum distace between kola 1000m
-kolaxy$disttoitself = Lib_DistEstimatesToItself(kolaxy$x, kolaxy$y)
-kolaxy2 = subset(kolaxy, kolaxy$disttoitself > 400)
-kxy <- as.data.frame(kolaxy2[c(1,2)])
+# # minimum distace between kola 1000m
+# kolaxy$disttoitself = Lib_DistEstimatesToItself(kolaxy$x, kolaxy$y)
+# kolaxy2 = subset(kolaxy, kolaxy$disttoitself > 100)
+# kxy <- as.data.frame(kolaxy2[c(1,2)])
 kxy$X <- kxy$x/1000 ; kxy$Y <- kxy$y/1000
 kXY <- kxy [c(3,4)]
 
-write.csv(kXY, "koalaxy.csv", row.names=FALSE)
-plot(kxy, add=TRUE)
-
-#read data from folder directly
-koalaxy <- read.csv("wartondata\\koalaxy.csv", header = TRUE)
-
-env <- read.csv("wartondata\\env.csv", header = TRUE)
+#kXY1<- kXY[sample(nrow(kXY), 100), ]
+# select points fall within the raster. rasterise a variable(arr). extract valuves with points. remove na records.
+abc <- extract(arr, kXY)
+abc1 <- as.data.frame(abc)
+abc2 <- cbind(abc1, kXY)
+abc2 <- na.omit(abc2)
+koalaxyT <- abc2[c(2,3)]
+# #read data from folder directly
+#koalaxy <- read.csv("wartondata\\koalaxy.csv", header = TRUE)
+# 
+# env <- read.csv("wartondata\\env.csv", header = TRUE)
 
 #########
-ppmForm = ~  poly(elev,temp, degree = 2) 
-ppmFit = ppmlasso(ppmForm, sp.xy = koalaxy, env.grid = env, sp.scale = s1)
+ppmForm = ~  poly(TEMP,ELEV, degree = 2) 
+ppmFit = ppmlasso(ppmForm, sp.xy = koalaxyT, env.grid = stt, sp.scale = 1)
 # To predict using model-based control of observer bias (at min value for D_MAIN_RDS):
-newEnv = sss
+newEnv = stt
 #newEnv$A.1 = min(stand.A.1)
 pred.biasCorrect = predict(ppmFit, newdata=newEnv)
 #newEnv$A.1 = min(stand.A.1)
-pred.biasCorrect.1 = predict(ppmFit, newdata=sss)
-predictions <- cbind(xydatan, pred.biasCorrect.1)
-xy.rr <- rasterFromXYZ(as.data.frame(predictions)[, c("X", "Y", "pred.biasCorrect.1")])
+#pred.biasCorrect.1 = predict(ppmFit, newdata=sss)
+predictions <- cbind(xydatan, pred.biasCorrect)
+xy.rr <- rasterFromXYZ(as.data.frame(predictions)[, c("X", "Y", "pred.biasCorrect")])
 plot(xy.rr, las=0)
 # To find the resolution (in the range from 0.5 to 16 km):
 scales = c(0.5, 1, 2, 4, 8, 16)
