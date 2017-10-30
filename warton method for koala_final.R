8d5ae83bce95664dec4df2c7da3be99463fbe8bd
Ravi Dissanayake <disabandara@gmail.com>
2017-10-30 17:04:38 +1000

    changed

--- warton method for koala_final.R
+++ warton method for koala_final.R
@@ -38,10 +38,13 @@ kolaxyT <- subset(kolaxy2, Y > 6902 & Y < 7000) # xy within the area only.
 #########
 ppmForm = ~  poly(temp,elev, degree = 1) 
 ppmFit = ppmlasso(ppmForm, sp.xy = kolaxyT, env.grid = stt, sp.scale = 1)
+
+
+
 # To predict using model-based control of observer bias (at min value for D_MAIN_RDS):
 newEnv = stt
 #newEnv$A.1 = min(stand.A.1)
-pred.biasCorrect = predict(ppmFit, newdata=newEnv)
+pred.biasCorrect = predict(ppmFit, newdata=newstt)
 #newEnv$A.1 = min(stand.A.1)
 #pred.biasCorrect.1 = predict(ppmFit, newdata=sss)
 predictions <- cbind(xydatan, pred.biasCorrect)
