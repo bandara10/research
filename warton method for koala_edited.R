661bf4482a72b6f54f1a6f3ff968b16aac7adcb5
Ravi Dissanayake <disabandara@gmail.com>
2017-10-30 13:14:44 +1000

    distance added no convergence.

--- warton method for koala_edited.R
+++ warton method for koala_edited.R
@@ -29,7 +29,14 @@ xydatan <- stt[c(1,2)]
 # plot(sbd)
 
 #quad.1 = sample.quad(env.grid =stt , sp.scale = 1, file = "Quad") # this is quadrature points to be use for the analysis.
+ 
+stand.distance_tertiaryandlink=scale.default(stt$distance_tertiaryandlink, center = TRUE, scale = TRUE) #standarise
+stt$distance_tertiaryandlink = stand.distance_tertiaryandlink
 
+# To predict using model-based control of observer bias (at min value for distance):
+newstt <- stt
+newstt$distance_tertiaryandlink = min(stand.distance_tertiaryandlink)
+pred.biasCorrect = predict(ppmFit, newdata=newstt)
 
 #koala data
 kolaxy <- read.csv("wartondata\\koalaxy.csv", header = TRUE) # in km.XY| go to ppmFrom
@@ -39,9 +46,9 @@ kolaxyT <- subset(kolaxy2, Y > 6902 & Y < 7000) # xy within the area only.
 ppmForm = ~  poly(temp,elev, degree = 1) + poly(distance_tertiaryandlink, degree = 1)
 ppmFit = ppmlasso(ppmForm, sp.xy = kolaxyT, env.grid = stt, sp.scale = 1)
 # To predict using model-based control of observer bias (at min value for D_MAIN_RDS):
-newEnv = stt
+
 #newEnv$A.1 = min(stand.A.1)
-pred.biasCorrect = predict(ppmFit, newdata=newEnv)
+pred.biasCorrect = predict(ppmFit, newdata=newstt)
 #newEnv$A.1 = min(stand.A.1)
 #pred.biasCorrect.1 = predict(ppmFit, newdata=sss)
 predictions <- cbind(xydatan, pred.biasCorrect)
