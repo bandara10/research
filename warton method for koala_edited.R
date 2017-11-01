86ab670db02f93bfa3281771633e9e3f6c6f77d0
Ravi Dissanayake <disabandara@gmail.com>
2017-11-01 21:22:56 +1000

    hpop added.

--- warton method for koala_edited.R
+++ warton method for koala_edited.R
@@ -5,12 +5,14 @@ library(rgdal)
 library(spatial.tools)
 # Pre-standardise observer bias variables
 setwd("C:\\Users\\uqrdissa\\ownCloud\\Covariates_analysis\\Mark_S\\raster_stack")
-myenv <- list.files( path="wartondata", pattern="\\.tif$", full.names = TRUE) 
+myenv <- list.files(path="wartondata", pattern="\\.tif$", full.names = TRUE) #path="wartondata", 
 myenv.stack <- stack(myenv)
 #crs(myenv.stack) <- NA
 extent(myenv.stack) <- extent(c(xmin(myenv.stack), xmax(myenv.stack), ymin(myenv.stack), ymax(myenv.stack))/1000)
 plot(myenv.stack[[1]])
+
 stt <- as.data.frame(myenv.stack, xy=TRUE, na.rm=T)
+#stt <- na.omit(stt)
 colnames(stt)[1] <- "X"
 colnames(stt)[2] <- "Y"
 # stt[is.na(stt)] <- 0
@@ -59,14 +61,15 @@ stt$dis_city = stand.dis_city
 
 #koala data
 kolaxyT <- read.csv("wartondata\\koalaxy.csv", header = TRUE) # in km.XY| go to ppmFrom
-
+#coordinates(kolaxyT) <- ~X+Y
+#plot(kolaxyT, add=TRUE)
 # kolaxy2 <- subset(kolaxy, X > 442 & X < 540)
 # kolaxyT <- subset(kolaxy2, Y > 6902 & Y < 7000) # xy within the area only.
 
 
 
 #########
-ppmForm1 = ~  poly(temp, elev,uhabit3, degree = 2) + poly(dis_visitor, degree = 2)
+ppmForm1 = ~  poly(temp, elev, hpop, degree = 2) +poly(dis_visitor, degree = 2) 
 ppmFit1 = ppmlasso(ppmForm1, sp.xy = kolaxyT, env.grid = stt, sp.scale = 1, n.fits = 200)
 
 pred.biasCorrectnot = predict(ppmFit1, newdata=stt)
@@ -80,7 +83,7 @@ plot(xy.rr)
 newstt <- stt
 newstt$stand.dis_visitor = min(stand.dis_visitor) # based on minimum distance
 
-ppmForm2 = ~  poly(temp,elev,uhabit3, degree = 2)+ poly(dis_visitor, degree = 2)
+ppmForm2 = ~  poly(temp,elev,hpop, degree = 2)+ poly(dis_visitor, degree = 2)
 ppmFit2 = ppmlasso(ppmForm2, sp.xy = kolaxyT, env.grid = stt, sp.scale = 1, n.fits = 200)
 pred.biasCorrect = predict(ppmFit2, newdata=newstt)
 predictions.correct <- cbind(xydatan, pred.biasCorrect)
@@ -101,9 +104,25 @@ resid.plot = diagnose(ppmFit2, which = "smooth", type = "Pearson")
 
 #####species interaction at 5km. NO avaiability grid is supplied here. 
 #species.int = point.interactions(dat.ppm, 5)
-ai.fit = ppmlasso(ppmForm1, data = dat.ppm, family = "area.inter", r = 1.5)
+ load("TestPPM.RData") # load dat.ppm
+ai.fit = ppmlasso(ppmForm1, data = dat.ppm, family = "area.inter", r = 3)
 diagnose(ai.fit, which = "smooth", type = "Pearson")
 pred.inter = predict(ai.fit, newdata=newstt)
 pred.inter <- cbind(xydatan, pred.inter)
 xy.inter <- rasterFromXYZ(as.data.frame(pred.inter)[, c("X", "Y", "pred.inter")])
 plot(xy.inter)
+writeRaster(xy.inter, "xy.tif")
+
+######
+bbr <- raster ("bbr.tif")  # bbr  is a dummy 1k resolution map not in meters.
+#use this map and fill with our raster data in to it as seperate new varibales. Becuase this chan be mapped and checked in arcgis.
+bbr <- as.data.frame(bbr, xy=TRUE)
+bbr <- bbr[c(-3)]
+elev <- as.data.frame(myenv.stack[[4]])
+ele <- cbind(bbr,elev)
+temp <- as.data.frame(myenv.stack[[5]])
+uhabit3 <- as.data.frame(myenv.stack[[6]])
+stt <- cbind(ele,temp,uhabit3)
+stt <- na.omit(stt)
+stt[] <- lapply(stt, as.integer)
+
