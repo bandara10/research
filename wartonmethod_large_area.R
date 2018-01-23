21ea1d5e727d9b50735391e29f866192f01474ba
bandara10 <disabandara@gmail.com>
2018-01-23 23:23:19 +1000

    detection model corrected for auc, lrtest AR

--- wartonmethod_large_area.R
+++ wartonmethod_large_area.R
@@ -2,13 +2,14 @@ library(maptools); library(sp); library(raster); library(rgdal); library(spatsta
 library(spatialkernel); library(splancs); library(RColorBrewer); library(dismo); library(spatial.tools)
 library(lmtest);library(spatial.tools);library(VGAM);library(mosaic);library(faraway);library(gstat)  #
 library(ncf);library(foreign);library(nlme)   ;library(MASS);library(ROCR);library(vcd)
-library(RColorBrewer);library(classInt);library(ppmlasso);library(usdm)
+library(RColorBrewer);library(classInt);library(ppmlasso);library(usdm) ; library(ncf); library(epicalc)
 
 ##############
 
 setwd("C:\\Users\\uqrdissa\\ownCloud\\Covariates_analysis\\Mark_S\\raster_syn\\warton_data")
 
 ##########Step 1: load koala data frm tow sources. BoalaBASE and Wildnet#####
+## step 1 and two are common to all methods.
 
 load("C://Users//uqrdissa//ownCloud//Covariates_analysis//Mark_S//Data_raw_koala//mydatasighting_cleaned.RData")
 mydata=mydatasighting_cleaned
@@ -41,7 +42,7 @@ habitat.r<- subset(myfullstack, c(1,2,4,6,13,16,23, 24, 25,26,27,28,29,39,40,41)
 
 #select koalas from grids
 
-selected.locations=gridSample(selected.locations, habitat.r, n = 15)
+selected.locations=gridSample(selected.locations, habitat.r, n = 100)
 loc=SpatialPoints((selected.locations))
 plot(loc, add=TRUE)
 
@@ -51,7 +52,7 @@ fun <- function() {
   plot(LGA.shp, add = TRUE, col = "red", pch = 1)
 }
 
-#plot rasters only
+#plot rasters only##
 
 plot(habitat.r,  nc = 4, nr =3)
 
@@ -71,6 +72,10 @@ habitat.rr<- subset(habitat.rr, c(1,2,15,18,26,33,39)) # select my variables
 #------------------------------------------------------------------------------------------
 
 ######Step 3. Create quadrature points and species xy data. Remove NA. Keep xy colomns for preditions and rasterize. ######
+
+big.grp <- as.data.frame(habitat.rr, xy=TRUE) # to be used later in Hefley method.
+
+#
 
 bigquad <- as.data.frame(habitat.rr, xy=TRUE,na.rm=TRUE)
 xydata <- bigquad[c(1,2)] ### use to get predictions and rasterise.
@@ -113,7 +118,7 @@ selected.locations.t=cbind(selected.locations,(extract(testlayer,selected.locati
 
 selected.locations<- as.data.frame(na.omit(selected.locations.t))
 
-
+selected.locations=selected.locations[c(1,2)]
 
 
 ###
@@ -333,7 +338,6 @@ myfullstack.subdf= as.data.frame(myfullstack.sub)
 
 #############
 
-
 # get all presence absences. Put all presences and mark them as 1 and all others zero. Then get 0/1.
 # This is the response data in iwlr and dwpr. Generally need presence locations only for other methods but
 #this is approximating ppm with logistic regression. So need 0/1.
@@ -359,15 +363,19 @@ rspec[cells] <- dpart$Haskoala
 
 plot(rspec)
 text(dpart$X, dpart$Y, lab = dpart$Haskoala)
-Press<- as.data.frame(rspec, xy=TRUE)   # Has snome NA valuves.
 
-#rename varibael awc
+Press.g<- as.data.frame(rspec, xy=TRUE)   # Has snome NA valuves.
 
-colnames(Press)[3] <- "koala"
-ptest=cbind(Press,myfullstack.subdf)   ### now remove na
+#### Press.g need this for Hefley method for group variable.
+
+#rename variable awc
+
+colnames(Press.g)[3] <- "koala"
+ptest=cbind(Press.g,myfullstack.subdf)   ### now remove na
 Press= na.omit(ptest)  # same as sp.at=Press ;  Pres=koala
 
 # save this for future use in comapringlikelihood.
+
 Press.my=Press
 
 # koala presence locations
@@ -467,8 +475,6 @@ for (i in 2:length(n.quad)){
 sp.dat.1=subset(Press.my, koala==1)
 sp.dat=sp.dat.1[c(-3)]
 colnames(sp.dat)[1] <- 'X'; colnames(sp.dat)[2] <- 'Y'
-
-
 #to compare likelihoods from iwlr , dwpr and ppmlasso. recall : same as sp.at=Press ;  Pres=koala
 
 # remember to chnage variable names.
@@ -494,6 +500,349 @@ plot(n.quad, loglik, log = "x", type = "o")
 
 #coudn`t get the Renners plot with all lines.
 
+
+#######   Hefley METHOD                              #########
+#                                                     #
+#            Use same dataset                         #
+#              Press.g
+#               habitat.rr from climatic data folder. ###
+
+
+#data required are : Koala location as 0 | over the entire study area.
+#                  :   raster data for above koala 0 | locations.
+# we saved data set "Press.my" has this variables.
+#  renamed object 
+
+# Press.g   # x, y, koala
+# big.grp # x,y variables
+#rename koala colomn to presence:
+
+
+#### This method is to correct group size so model. same covariate possible.
+# create a group variable and make it 1all locations. Otehrwise aggregate koalas in a grid and consider them as one unit or group..
+
+# r is an empty raster creaated above.
+
+group.r=rasterize(selected.locations, r) # 
+
+plot(group.r)
+
+group.r[is.na(group.r[])] <- 0 
+
+group.rr=group.r+r
+
+plot(group.rr)
+
+group=getValues(group.rr)
+
+# remove xy from big.grp  data
+
+myFDc=cbind(Press.g,group,big.grp)
+myFDc=myFDc[c(-1,-2)]
+
+myFD1=na.omit(myFDc)
+colnames(myFD1)[1] <- "presence"
+
+#### Step 4: select presence and abnsences data and combine with. #### 
+ZTGLM.myFD1=myFD1[which(myFD1$presence==1),] # select presence data. THis will be reorganised as 0 and 1 later.
+ZTGLM.myFD2=myFD1[which(myFD1$presence==0),] # select all absence data 
+
+#####Step 5: select only 1000 absences (monticarlo points as in hefleys method??)####
+set.seed(123)                                   # size data comes from absence locations.
+ZTGLM.myFD3 <- sample(seq_len(nrow(ZTGLM.myFD2)), size = 1000,replace=FALSE)#select only 1000 absences use for ipp.data
+ZTGLM.myFD4 <- ZTGLM.myFD2[ZTGLM.myFD3, ] #x.int data frame now
+#This is  similar to hefley`s IWLR data set.
+ZTGLM.myFD5=rbind(ZTGLM.myFD1,ZTGLM.myFD4) 
+
+##### Step 6: now take a random sample of 80 and assign detected 1 non detected 0.####
+
+train <- sample(seq_len(nrow(ZTGLM.myFD1)), size = 300,replace=FALSE)
+detected <- ZTGLM.myFD1[train, ]
+notdetected <- ZTGLM.myFD1[-train,] 
+#not detected assigned valuve 0
+notdetected$presence <- 0
+
+##### Step 7: Create the final data sets for the analysis####
+
+Detection.data= rbind(detected,notdetected) 
+str(Detection.data)
+
+IPP.data=rbind(detected, ZTGLM.myFD4) #IPP.data comes from detected data plus no koalas data from myFD1.
+
+ZTGLM.data=(detected)##ZTGLM.data# get the 80 rows selected.
+
+
+IPP.ignored=glm(presence~Annual_Mean_Temperature
+                + Annual_Precipitation
+                + Max_Temperature_of_Warmest_Month
+                + Min_Temperature_of_Coldest_Month
+                + fpcnew
+                ,family="binomial",weights=100000^(1-presence),data=IPP.data) # IPP.data2 added.
+
+
+
+summary(IPP.ignored)
+detecPress.gk=subset(Detection.data, select=c(-2))
+Press.gk=as.data.frame(Press.gk)
+
+# # ROC curve & GOF metrics
+# myPred = prediction(predict(Detection.model, type = "response"), Press.gk$koala)
+# perf <- performance(myPred, measure = "tpr", x.measure = "fpr")
+# plot(perf, colorize = T)
+# myPredPerfs = myLib.EstimateLogisticGOF(predict(myGLM, type = "response"), Press.gk$koala, 0.5)
+# myPredPerfs
+
+
+#############  
+set.seed(125)
+#Detection model: steps as in Hefley`s code`
+Detection.model=glm(presence~ distance_primaryandlink
+                    +distance_motorwayandlink
+                    + fpcnew
+                    ,family= "binomial"
+                    , data=Detection.data)
+
+unclass(summary(Detection.model))
+
+###################### Improve the model autocorrelation check and residual approach.#####
+#  got to step 4 if want to ignore this section
+######## Detection modelimproved by adding 
+# 1. Correlogrm prepared, Moran`s` I
+# 2. spatial autocorelation corrected using residulas.
+# 3. ROC curve 
+# 4. LRtest 
+###               ###               ###           ### 
+Detection.data$res = residuals(Detection.model)
+
+myResCorr <- correlog(Detection.data$x, Detection.data$y, Detection.data$res, na.rm=T, increment=10000, resamp=0, latlon = F)
+
+plot(myResCorr$mean.of.class[1:20], myResCorr$correlation[1:20] ,type="b", pch=16, lwd=1.5, cex = 1.2,xlab="distance", ylab="Moran's I")
+
+abline(h=0)
+
+
+
+# Crase approach to account for SA
+#############################################################
+myStack = stack(habitat.rr)
+# Plot the predictors
+plot(myStack)
+# Creates a mask layer
+myMask = myStack$Annual_Precipitation
+plot(myMask)
+# Map predictions
+AR1 = myMask * 0
+plot(AR1)
+AR1@file@name = "AR1"
+# Add it to the stack
+myARStack = addLayer(myStack, AR1)
+names(myARStack)[nlayers(myARStack)]="AR1"
+
+# Crase approach to account for SA
+#############################################################
+# Build the standard GLM object
+source("autoregresive2.r")
+Detection.data$AR1 = myLib.AutoRegressiveMean(residuals(Detection.model), Detection.data$x, Detection.data$y, 15000)
+
+
+Detection.model.2=glm(presence~ distance_primaryandlink
+                      +distance_motorwayandlink
+                      + fpcnew
+                      + AR1
+                      ,family= "binomial"
+                      ,data=Detection.data)
+summary(Detection.model.2)
+
+
+plot(Detection.model.2)
+
+Detection.data$R1 = residuals(Detection.model.2)
+Corr <- correlog(Detection.data$x, Detection.data$y, Detection.data$R1,na.rm=T, increment=10000, resamp=0, latlon = F)              
+#Corr <- correlog(myFD$x, myFD$y, myFD$R1,na.rm=T, increment=10000, resamp=0, latlon = F)
+#mySigVec = ifelse(Corr$p<0.01,1,0)  
+
+#lines(Corr$mean.of.class[1:20], Corr$correlation[1:20], col = "red")
+#points(Corr$mean.of.class[1:20], Corr$correlation[1:20], pch = 16, col = mySigVec[1:20]+1)
+# Plot the correlogram
+plot(Corr) 
+# Estimates DAIC for each predictor
+#############################################################
+library(epicalc)
+myPredictors=c("distance_motorwayandlink","distance_primaryandlink","fpcnew")
+myChi2 = rep(0,length(myPredictors))
+myPValue = rep(0,length(myPredictors))
+
+for (i in 1:length(myPredictors)){
+  myShortPred = myPredictors[-i]
+  myStrN = "presence ~"
+  for (j in 1:length(myShortPred)){myStrN = paste(myStrN, "+", myShortPred[j])}
+  myStrARN = paste(myStrN,"+ AR1")
+  Detection.model.2 = glm(as.formula(myStrARN), data = Detection.data, family = "binomial")
+  mylrtest = lrtest(Detection.model,Detection.model.2)
+  myChi2[i] = mylrtest[[4]]
+  myPValue[i] = mylrtest[[6]]
+}
+myLRTable = data.frame(myChi2,myPValue)
+myLRTable = cbind(myPredictors,myLRTable)
+myLRTable
+
+# ROC & predictions
+#############################################################
+# Map predictions
+AR1 = myMask * 0
+AR1@file@name = "AR1"
+# Add it to the stack
+myARStack = addLayer(myStack, AR1)
+names(myARStack)[nlayers(myARStack)]="AR1"
+
+myPred = predict(myARStack, Detection.model.2, type = "response")
+#  myPred = predict(myStack, myGLM, type = "response")
+
+# plot the prediction
+plot(myPred, xlab = "x-coord (UTM45)", ylab= "y-coord (UTM45)",main=" detection koala")
+
+# ROC curve & GOF metrics
+myPred = prediction(predict(Detection.model.2, type = "response"), Detection.data$presence)
+perf <- performance(myPred, measure = "tpr", x.measure = "fpr")
+plot(perf, colorize = T)
+myPredPerfs = myLib.EstimateLogisticGOF(predict(Detection.model.2, type = "response"), Detection.data$presence, 0.5)
+myPredPerfs
+
+
+
+#####################
+
+#####Step 4: Estimate the probability of detection for each presence-only location.####
+p.det=ilogit(predict(Detection.model.2,new=ZTGLM.data))# chnaged myD to ZTGLM.data length =461. 3 X=vector.boot 
+
+# set very small valuves to 0.1
+p.det <- ifelse(p.det<0.1,0.1,p.det)
+
+hist(p.det, breaks=70)
+
+
+# p.det 1 or very low valuves create convergencce issues.
+IPP.data$p.det=c(p.det,rep(1,length(ZTGLM.myFD3)))
+#IPP.data$p.det=p.det   # IPP.data number of obserarions=1461
+ZTGLM.data$p.det=p.det
+
+######Step 5: - Fit an inhomogeneous Poisson point process  that weights the log-likelihood by 1/p.det . ####
+#use step function here then use significant variable in the next model. or else go to line 79. I asume this is correct way to do it.
+IPP.corrected=glm(presence~Annual_Mean_Temperature
+                + Annual_Precipitation
+                + Max_Temperature_of_Warmest_Month
+                + Min_Temperature_of_Coldest_Month
+                + fpcnew
+                ,weights=(1/p.det)*10000^(1-presence),family="binomial",  data=IPP.data)
+
+
+summary(IPP.corrected)
+####Step 6: Fit an zero-truncated Poisson generalized linear model that weights the log-likelihood by 1/p.det.####
+
+#use only the significant covariates, tpo +hpop+lot_density+sbd
+ZTGLM.corrected=vglm(presence~ distance_primaryandlink
+                    +distance_motorwayandlink
+                    + fpcnew
+                    ,weights=1/p.det,family="pospoisson",data=ZTGLM.data)
+summary(ZTGLM.corrected)
+
+# step 7:  Detion model Map predictions
+
+myPred = predict(habitat.rr, Detection.model, type = "response")
+plot(myPred, xlab = "x", ylab= "y",main="detection model")
+
+#        IPP ignored model Map predictions
+
+myPred2 = predict(habitat.rr, IPP.ignored, type = "response")
+plot(myPred2, xlab = "x", ylab= "y",main=" IPP model |detection ignored- number of koalas")
+
+#        IPP corrected model Map predictions
+
+myPred2 = predict(habitat.rr, IPP.corrected, type = "response")
+plot(myPred2, xlab = "x", ylab= "y",main=" IPP model |detection considered- number of koalas")
+
+myPred3 = predict(habitat.rr, ZTGLM.corrected, type = "response")
+plot(myPred3,  main="ZTGLM-group size koalas in a grid model")
+
+
+#	Calculating mean, standard deviation and 95%, equal-tailed confidence intervals
+#	from the empirical distribution. See "Introduction to the Bootstrap" (Efron & Tibshirani 1994) 
+#	for more details.
+#
+#     Estimates of the coefficient for the covariate "x" (0.9520575) of the inhomogeneous Poisson point 
+#	process model and  estimate the intercept(0.9832485) and covariate "x" (0.531719) of the zero-truncated 
+#	Poisson generalized linear model are close to the true values (1, 1, 0.5).   
+#
+#	Standard errors when detection bias is accounted for(0.1141398; 0.03696048; 0.0224879), and thus 95%  
+#	confidence intervals, and standard errors (0.01185; 0.0099313; 0.0060487) are larger when the variability  
+#	in the probability of detection is not accounted for. Note the estimated standard errors(0.01185; 0.0099313; 0.0060487)
+#	are obtained from steps 5 & 6 above.	
+
+####### step 8: Two phase algoritham#######################################################################
+
+#### chnage below variable list based on s=identified variables in each model.
+
+set.seed(1234)
+tpnbs=function()	{
+  bss=resample(1:dim(ZTGLM.data)[1])
+  IPP.data.bss=rbind(IPP.data[bss,1:53],IPP.data[which(IPP.data$presence==0),1:53])
+  ZTGLM.data.bss=ZTGLM.data[bss,]
+  
+  Detection.data.bss=resample(Detection.data)
+  
+  Detection.model=glm(presence ~distance_pedestrian+s1_residential_dist+distance_trunkandlink+
+                        distance_tertiaryandlink+scale(group),family="binomial",data=Detection.data.bss)
+  
+  p.det.bss=ilogit(predict(Detection.model,new=ZTGLM.data.bss))
+  IPP.data.bss$p.det=c(p.det.bss,rep(1,length(ZTGLM.myFD3))) 
+  ZTGLM.data$p.det=p.det.bss
+  options(warn=-1)
+  
+  IPP.model= glm(presence~ twi+tpo+temp+aspect+elev+habit2pc+hpop+lot_density+sbd, 
+                 family = "binomial", weights = (1/p.det)*10000^(1-presence), data = IPP.data.bss)
+  
+  ZTGLM.corrected=vglm(group~twi+tpo+temp+aspect+elev+habit2pc+hpop+lot_density+sbd,
+                       weights=1/p.det,family="pospoisson",data=ZTGLM.data.bss)
+ 
+   c(coef(IPP.model),coef(ZTGLM.corrected))
+}
+
+#### step 9: boostrapping ####
+n.bootstraps=1000
+bootstrap.sample = mosaic::do(n.bootstraps)*tpnbs() # Ravi changed the moasic::do and tpnbs()
+bootstrap.sample <- data.frame(bootstrap.sample)
+#bootstrap.sample=as.data.frame (bootstrap.sample) #Ravi created a matrix from the dataframe
+######## each step gives mean of coefficients for intercept or covariates
+save.image(file="hefleydatapreparation.v3.3.RData")
+#### step 10:  Get means and sd ####
+colMeans(bootstrap.sample)[1]
+sd(bootstrap.sample)[1]
+qdata(c=(.025),bootstrap.sample[,1]) # ravi:removed after .975 after .025, add c=
+
+colMeans(bootstrap.sample)[2] 
+sd(bootstrap.sample)[2] 
+qdata(c=(.025),bootstrap.sample[,2])#ravi:removed after .975 after .025, add c=
+
+colMeans(bootstrap.sample)[3] 
+sd(bootstrap.sample)[3] 
+qdata(c=(.025),bootstrap.sample[,3])#ravi:removed after .975 after .025, add c=
+
+colMeans(bootstrap.sample)[4]
+sd(bootstrap.sample)[4]
+qdata(c=(.025),bootstrap.sample[,4])#ravi:removed after .975 after .025, add c=
+
+colMeans(bootstrap.sample)[5]
+sd(bootstrap.sample)[5]
+qdata(c=(.025),bootstrap.sample[,5])#ravi:removed after .975 after .025, add c=
+
+
+
+
+
+######   Drorazio method  ##############
+
+          #use same data 
+####  ####  ####    ###  ####  ####  ####    ###
 
 
 
